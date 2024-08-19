2fd2604fadf46da05962ceef5aba8e05560f2647
Datayano <datayan@tuta.io>
2024-08-20 01:36:01 +0200

    Modification du fichier README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+Merci d'entrer votre identité dans un fichier .env à la racine du projet.
+Suivez le même template tel qu'indiqué dans sample.env.
+Nous imprimerons votre billet dès que nous aurons ces informations... Bon voyage !
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 // server.js
-import "dotenv/config"; // this line is mandatory when you want to read variables from the '.env' file
-console.log('Avant de commencer votre nouvelle mission, veuillez vérifier votre nouvelle identité.')
+import "dotenv/config";
+
+console.log('Avant d\'embarquer, nous allons vérifier votre identité.');
 console.log(`Nom : ${process.env.NOM}`);
 console.log(`Prénom : ${process.env.PRENOM}`);
 console.log(`Date de naissance : ${process.env.DATE_DE_NAISSANCE}`);
-console.log(`Nationalité : ${process.env.NATIONALITE}`);
+console.log(`Destination : ${process.env.DESTINATION}`);
+
+
+setTimeout(() => {
+    console.log(``);
+    console.log(`Tout semble correcte !`);
+    console.log(`Très bon choix de destination.`);
+    console.log(`Bon voyage ${process.env.PRENOM} !`)
+}, 3000);
+
+let dots = 0;
+const interval = setInterval(() => {
+    process.stdout.write('.');
+    dots++;
+    if (dots > 10) {
+        process.stdout.write('\n');
+        clearInterval(interval);
+    }
+}, 300);
